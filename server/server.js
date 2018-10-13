677fcda2d64f1a5c497787995c02d530ce2e62df
Humad <humad@live.com>
2018-10-13 18:57:05 -0400

    switched to mongodb

--- server/server.js
+++ server/server.js
@@ -2,10 +2,28 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + './../build'));
+
+const MLAB_URI = "mongodb://memers:urmomgay69@ds131763.mlab.com:31763/meme-curator"
+  
+mongoose.connection.on('connected', function() {
+    console.log('Success: connected to MongoDb!');
+});
+mongoose.connection.on('error', function(err) {
+    console.log('Error connecting to MongoDb: ' + err);
+    process.exit(1);
+});
+
+mongoose.connection.on("connected", function() {
+    console.log("Connected to mlab");
+});
+
+mongoose.connect(MLAB_URI);
+
 
 // Start server
 app.listen(port, function() {
