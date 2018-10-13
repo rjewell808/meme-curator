3738c96c33c027b9ecbf28364fd3cd59e3b542e7
Humad <humad@live.com>
2018-10-13 16:12:51 -0400

    added reddit api support

--- server/apiRoutes.js
+++ server/apiRoutes.js
@@ -2,6 +2,25 @@ const router = require('express').Router();
 const db = require('./database');
 const request = require('request');
 
+const memeWeights = {
+    "memes":                50,
+    "dankmemes":            50,
+    "greentext":            50,
+    "wholesomememes":       50,   
+    "dankchristianmemes":   50,
+    "meirl":                50,
+    "blackpeopletwitter":   50,
+    "whitepeopletwitter":   50,
+    "me_irl":               50,
+    "prequelmemes":         50,
+    "adviceanimals":        50,
+    "deepfriedmemes":       50,
+    "programmerhumor":      50,
+    "i_irl":                50,
+    "bikinibottomtwitter":  50,
+    "boottoobig":             50
+}
+
 router.get('/addFavorite', function(req, res) {
 
 });
@@ -11,20 +30,65 @@ router.get('/getFavorites', function(req, res) {
 });
 
 router.get('/getAllMemes', function(req, res) {
-    getAllMemes()
-    .then()
+    getAllMemes().then(function(data) {
+        let result = [];
+        result.concat(...data);
+        res.json({
+            result: result
+        });
+    });
 });
 
 function getAllMemes() {
-    const subreddits = ["memes"];
-    for (var i = 0; i < subreddits.length; i++) {
-        const currentsubreddit = subreddits[i];
-        request(`http://www.reddit.com/r/${currentsubreddit}/.json`, function(error, response, body) {
-            console.log(response);
-            console.log("ZZZZZZZZZZZZZZZZ");
-            console.log(body);
+    return new Promise(function(resolve, reject) {
+        console.log("Getting all memes")
+        let allPromises = [];
+        const subreddits = Object.keys(memeWeights);
+        for (var i = 0; i < subreddits.length; i++) {
+            const currentsubreddit = subreddits[i];
+            allPromises.push(getMemeFromSubreddit(currentsubreddit));
+        }
+    
+        Promise.all(allPromises)
+        .then(function(data) {
+            console.log("All promises resolved")
+            resolve(data);
+        })
+        .catch(function(error) {
+            reject(error);
         });
+    });
+}
+
+function getMemeFromSubreddit(subreddit) {
+    console.log("About to make request for", subreddit);
+    return new Promise(function(resolve, reject) {
+        let memes = []
+        request(`http://www.reddit.com/r/${subreddit}/.json`, function(error, response, body) {
+            if (error) {
+                reject(error);
+            } else {
+                const children = JSON.parse(body).data.children;
+                let j = 0;
+                while (j < children.length && memes.length < 5) {
+                    if (children[j].data.post_hint === "image") {
+                        memes.push({imageUrl: children[j].data.url, subreddit: subreddit});
+                    }
+                    j++;
+                }
+                console.log("Got memes for", subreddit);
+                resolve(memes);
+            }
+        });
+    })
+}
+
+function getSum() {
+    let sum = 0;
+    for (let key in memeWeights) {
+        sum += memeWeights[key];
     }
+    return sum;
 }
 
 router.get('/getMemeOfType/:memeType', function(req, res) {
@@ -47,8 +111,6 @@ function insertFavorite() {
         console.log(result);
     });
 }
-
-getAllMemes();
 
 
 
